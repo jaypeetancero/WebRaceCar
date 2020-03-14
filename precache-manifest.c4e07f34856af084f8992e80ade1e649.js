75cbdcf246dabb3ece17e089b3cf0fb1d28cc4ae
jaypeetan <jaypee.tan@codegum.ph>
2020-03-14 17:25:26 +0800

    Updates

---
+++ precache-manifest.c4e07f34856af084f8992e80ade1e649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b85fdef42f33af39d5cf4b1382ea57",
+    "url": "/WebRaceCar/index.html"
+  },
+  {
+    "revision": "dca37b21d7020c4c2b09",
+    "url": "/WebRaceCar/static/css/main.2d5b2c7d.chunk.css"
+  },
+  {
+    "revision": "34a0db480ef60397009f",
+    "url": "/WebRaceCar/static/js/2.7d6b067b.chunk.js"
+  },
+  {
+    "revision": "92993e1feaccc30e8e07764395cfe931",
+    "url": "/WebRaceCar/static/js/2.7d6b067b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca37b21d7020c4c2b09",
+    "url": "/WebRaceCar/static/js/main.3f3144b5.chunk.js"
+  },
+  {
+    "revision": "5fb99a962d2ddf86f111",
+    "url": "/WebRaceCar/static/js/runtime-main.8d1a0ab8.js"
+  }
+]);
